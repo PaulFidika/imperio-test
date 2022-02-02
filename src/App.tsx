3b6b6cd21f4f7d28d2f5549842d6a593dbda6467
Paul Fidika <paul@fidika.com>
2022-02-01 23:43:46 -0700

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+// import { ReactComponent as Knight } from './knight.svg'
+import Knight from './KnightMotion'
+
+const animation = {}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Knight className="knight" />
+        {/* <img src={knight} className="knight" alt="knight" /> */}
+        <p>Imperio DAO</p>
+        This is written in English
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
